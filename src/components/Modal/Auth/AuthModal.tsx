8e1d81c0783984d42642673c5e9016b0a2816c09
이준형 <dlwnsgud261@naver.com>
2023-02-25 01:15:56 +0900

    AuthInputs 컴포넌트 가져오기

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,6 +1,7 @@
 import { authModalState } from '@/src/atoms/authModalAtom';
 import React from 'react';
 import { useRecoilState } from 'recoil';
+import AuthInputs from './AuthInputs';
 
 const AuthModal:React.FC = () => {
     const [modalState, setModalState] = useRecoilState(authModalState);
@@ -22,7 +23,9 @@ const AuthModal:React.FC = () => {
                     {/*header*/}
                     <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                         <h3 className="text-3xl font-semibold">
-                            Modal Title
+                            {modalState.view === "login" && "Login"}
+                            {modalState.view === "signup" && "Sign Up"}
+                            {modalState.view === "resetPassword" && "Reset Password"}
                         </h3>
                         <button
                         className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
@@ -34,14 +37,10 @@ const AuthModal:React.FC = () => {
                     </div>
                         {/*body*/}
                         <div className="relative p-6 flex-auto">
-                        <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                        I always felt like I could do anything. That’s the main
-                        thing people are controlled by! Thoughts- their perception
-                        of themselves! They're slowed down by their perception of
-                        themselves. If you're taught you can’t do anything, you
-                        won’t do anything. I was taught I could do everything.
-                    </p>
-                </div>
+                            {/* <OAuthButtons /> */}
+                            <AuthInputs />
+                            {/* <ResetPassword /> */}
+                        </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
