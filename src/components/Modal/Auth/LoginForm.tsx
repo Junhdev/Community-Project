53876c8405b99a4bca674c6232255291a79b3ed3
이준형 <dlwnsgud261@naver.com>
2023-02-24 23:57:51 +0900

    useInput 받아와서 디자인하기

---
+++ src/components/Modal/Auth/LoginForm.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import useInput from "@/src/hooks/useInput";
+
+
+const LoginForm:React.FC = () => {
+    const email = useInput("");
+    const password = useInput("");
+
+    const submitForm = (event) => {
+        event.preventDefault();
+        console.log("email", email.value);
+        console.log("password", password.value);
+    };
+
+    return (
+        <form onSubmit={submitForm}>
+            <input placeholder="Email" {...email /*email 칸에 대한 return값 불러오기 즉, value={inputValue} onChange={handleChange} */ } />
+            <input placeholder="Password" type="password" {...password} />
+        </form>
+    );
+};
+
+export default LoginForm;
