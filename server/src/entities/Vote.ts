fd528d79621a551535c2d4cc34f76a4aa518c214
이준형 <dlwnsgud261@naver.com>
2023-03-07 00:20:24 +0900

    like Entity 생성

---
+++ server/src/entities/Vote.ts
@@ -0,0 +1,35 @@
+import BaseEntity from "./Entity";
+import { Entity, Column, JoinColumn, ManyToOne } from "typeorm";
+import { User } from "./User";
+import Post from "./Post";
+import Comment from "./Comment";
+
+
+
+
+@Entity('votes')
+export default class Vote extends BaseEntity{
+    @Column()
+    value: number;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: "username", referencedColumnName: "username" })
+    user: User
+
+    @Column()
+    username: string;
+
+    /* comment에 대한 vote일때 */
+    @Column({ nullable: true })
+    postId: number;
+
+    @ManyToOne(() => Post)
+    post: Post;
+
+    /* post에 대한 vote일때 */
+    @Column({ nullable: true })
+    commentId: number;
+
+    @ManyToOne(() => Comment)
+    comment: Comment
+}
