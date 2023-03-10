821f84d487b3f9f4c7f268eecc5c05125da57bd5
이준형 <dlwnsgud261@naver.com>
2023-03-10 23:53:23 +0900

    수정

--- client/src/components/Modal/Auth/AuthInputs.tsx
+++ client/src/components/Modal/Auth/AuthInputs.tsx
@@ -4,15 +4,20 @@ import { authModalState } from '@/src/atoms/authModalAtom';
 import LoginForm from './LoginForm';
 import SignUp from './SignUp';
 
-
+/* AuthModal 컴포넌트의 body 부분에 렌더링 되는 컴포넌트 */
 const AuthInputs:React.FC = () => {
     const modalState = useRecoilValue(authModalState);
     
     return (
         <div>
-        {modalState.view === "login" && <LoginForm />}
-        {modalState.view === "signup" && <SignUp />}
-        {/*modalState.view === "resetPassword" && "Reset Password"*/}
+            {/* view값에 따른 렌더링 설정 */}
+            {/* view값이 login일때 로그인폼 띄어주기 */}
+            {modalState.view === "login" && <LoginForm />}
+
+            {/* view값이 signup일때 회원가입창 띄어주기 */}
+            {modalState.view === "signup" && <SignUp />}
+            
+            {/*modalState.view === "resetPassword" && "Reset Password"*/}
         </div>
     )
 }
