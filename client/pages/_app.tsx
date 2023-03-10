65ed32815cb904476da27af916a093e15b51e130
이준형 <dlwnsgud261@naver.com>
2023-03-11 01:17:00 +0900

    수정

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,8 +2,10 @@ import type { AppProps } from 'next/app'
 import Layout from 'src/components/Layout/Layout'
 import 'styles/globals.css';
 import { RecoilRoot } from 'recoil';
+import Axios from 'axios';
 
 export default function App({ Component, pageProps }: AppProps) {
+  Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api"
   return (
     <RecoilRoot>
       <Layout>
