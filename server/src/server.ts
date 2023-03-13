cb9dbc2f6b4f735dd185c472c9c0b85b5bb223be
이준형 <dlwnsgud261@naver.com>
2023-03-13 23:57:17 +0900

    Entry 파일 수정

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,21 @@
+/* entry 파일 */
 import express from "express";
 import morgan from "morgan";
 import { AppDataSource } from "./data-source";
+import authRoutes from './routes/auth';
+
 
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
+/* for body-parsing application/json */
 app.use(express.json());
+/* HTTP 요청에 대한 log를 남겨주는 미들웨어 */
 app.use(morgan('dev'));
 
 app.get("/", (_, res) => res.send("running"));
+app.use("/api/auth", authRoutes);
 
 let port = 4000;
 app.listen(port, async () => {
