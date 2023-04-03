e0e7558d03e917c09f35b751a89aac06544e1b35
이준형 <dlwnsgud261@naver.com>
2023-04-03 17:01:41 +0900

    Navbar signup페이지와 login페이지에서 안나타나게 설정

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,11 +1,16 @@
-
 import React from 'react';
 import Navbar from 'src/components/Navbar/Navbar';
+import { useRouter } from 'next/router';
+
+const { pathname } = useRouter();
+const authRoutes = ["/signup", "/login"];
+const authRoute = authRoutes.includes(pathname);
 
 const Layout = ({ children } : any) => {
     return (
         <>
-        <Navbar />
+        {/* Navbar는 signup페이지와 login페이지에서 안나타나게 설정 */}
+        {!authRoute && <Navbar />}
         <main>{children}</main> 
         </>
     );
@@ -18,20 +23,3 @@ export default Layout;
 
 
 
-
-/* 
-import React from 'react';
-import Navbar from 'src/components/Navbar/Navbar';
-
-const Layout = ({ children }) => {
-    return (
-        <>
-        <Navbar />
-        <main>{children}</main> 
-        </>
-    );
-};
-
-export default Layout;
-
-*/
