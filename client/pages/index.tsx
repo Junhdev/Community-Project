c90a06b3c4119b164a1005a7bf8e2e353cca6dd1
이준형 <dlwnsgud261@naver.com>
2023-04-04 23:56:33 +0900

    메인화면 생성

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,46 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import useSWR from 'swr';
+import axios from 'axios';
+import { Community } from '@/src/types';
+import Link from 'next/link';
 
+const Home = () => {
+  const fetcher = async (url: string) => {
+    return await axios.get(url).then(res => res.data)
+  }
+  const address = `/subs/sub/topCommunity`;
+  const { data: topCommunities } = useSWR<Community[]>(address, fetcher)
 
-export default function Home() {
   return (
-    <div className="text-3xl font-bold underline">
-      asdasd
-    </div>
+    <div className='flex max-w-5xl px-4 pt-5 mx-auto'>
+      {/* 포스트 리스트 */}
+      <div className='w-full md:mr-3 md:w-8/12'>
+        
+
+      </div>
+
+      {/* 사이드바 */}
+      <div className='hidden w-4/12 ml-3 md:block'>
+        <div className='bg-white border rounded'>
+          <div className='p-4 border-b'>
+            <p className='text-lg font-semibold text-center'>상위 커뮤니티</p>
+          </div>
+
+          {/* 커뮤니티 리스트 */}
+          <div>
+           
+          <Link href="/communities/create">
+                
+              </Link>
+                
+              </div>
+              </div>
+              </div>
+              </div>
+            
   )
 }
+
+export default Home;
+
