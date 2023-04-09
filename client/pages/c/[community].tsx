a996a9c289e553aca1ad7bb9d13d78459eb903ea
이준형 <dlwnsgud261@naver.com>
2023-04-09 22:46:31 +0900

    이미지 업로드를 위해 useRef를 이용하여 관련 함수 생성

--- client/pages/c/[community].tsx
+++ client/pages/c/[community].tsx
@@ -1,12 +1,53 @@
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import useSWR from 'swr';
 import Image from 'next/image';
+import { useAuthState } from '@/src/context/auth';
 
 
 const CommunityPage: React.FC = () => {
+    const [myCommunity, setMyCommunity] = useState(false);
+    const { authenticated, user } = useAuthState();
 
+    useEffect(() => {
+        if (!community || !user) return;
+        // 로그인 되어있고 해당 유저가 해당 커뮤니티를 만든 유저인지 판별 >> 맞다면 true 반환
+        setMyCommunity(authenticated && user.username === community.username);
+    }, [community]);
+
+    // 이미지 업로드 DOM 선택
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const openFileInput = (type: string) => {
+        if(!myCommunity) return;
+        const fileInput = fileInputRef.current;
+        if(fileInput) {
+            fileInput.name = type;
+            fileInput.click();
+        }
+    }
+
+    // 백엔드에 이미지 업로드 요청 보내기
+    const uploadImage = async (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files === null) return;
+
+        const file = event.target.files[0];
+        console.log('file', file); 
+
+        const formData = new FormData();
+        formData.append("file", file);
+        // type error를 !로 제거(fileInputRef.current가 not null인 것이 확실하기 때문)
+        formData.append("type", fileInputRef.current!.name);
+
+        try {
+            await axios.post(`/communities/${community.name}/upload`, formData, {
+                headers: { "Context-Type": "multipart/form-data" }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
     // 커뮤니티별 페이지 데이터 가져오기
     const fetcher = async (url: string) => {
         try {
@@ -31,7 +72,7 @@ const CommunityPage: React.FC = () => {
             {community &&
                 <>
                     <div>
-                        
+                        <input type="file" hidden={true} ref={fileInputRef} onChange={uploadImage} />
                         {/* 배너 이미지 */}
                         <div className="bg-gray-400">
                             {community.bannerUrl ? (
@@ -43,11 +84,12 @@ const CommunityPage: React.FC = () => {
                                         backgroundSize: 'cover',
                                         backgroundPosition: 'center',
                                     }}
-    
+                                    onClick={() => openFileInput("banner")}
                                 >
                                 </div>
                             ) : (
                                 <div className='h-20 bg-gray-400'
+                                onClick={() => openFileInput("banner")}
                                 ></div>
                             )}
                         </div>
@@ -64,7 +106,7 @@ const CommunityPage: React.FC = () => {
                                             width={70}
                                             height={70}
                                             className="rounded-full"
-                                            
+                                            onClick={() => openFileInput("profile")}
                                         />
                                     )}
                                 </div>
@@ -74,7 +116,7 @@ const CommunityPage: React.FC = () => {
                                         <h1 className='text-3xl font-bold '>{community.title}</h1>
                                     </div>
                                     <p className='font-bold text-gray-400 text-small'>
-                                        /r/{community.name}
+                                        {community.name}
                                     </p>
                                 </div>
                             </div>
