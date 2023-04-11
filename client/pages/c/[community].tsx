74a935da4c281ab1667eb87c01bb0b1016a5ff19
이준형 <dlwnsgud261@naver.com>
2023-04-11 23:54:53 +0900

    커뮤니티 상세페이지의 사이드바 생성

--- client/pages/c/[community].tsx
+++ client/pages/c/[community].tsx
@@ -4,28 +4,39 @@ import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import useSWR from 'swr';
 import Image from 'next/image';
 import { useAuthState } from '@/src/context/auth';
+import SideBar from '@/src/components/SideBar';
 
 
 const CommunityPage: React.FC = () => {
     const [myCommunity, setMyCommunity] = useState(false);
     const { authenticated, user } = useAuthState();
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    
+    // 커뮤니티별 페이지 데이터 가져오기
+    const fetcher = async (url: string) => {
+        try {
+            const res = await axios.get(url);
+            return res.data;
+        } catch (error: any) {
+            throw error.response.data
+        }
+    }
+    const router = useRouter();
+    console.log(router);
+
+    // router에 query로 url정보가 담겨있으므로
+    // router.query { community: 'test1' }
+    const communityname = router.query.community;
+
+    
+    const { data: community, error } = useSWR(communityname ? `/communities/${communityname}` : null, fetcher);
+    console.log('community', community);
 
     useEffect(() => {
         if (!community || !user) return;
         // 로그인 되어있고 해당 유저가 해당 커뮤니티를 만든 유저인지 판별 >> 맞다면 true 반환
         setMyCommunity(authenticated && user.username === community.username);
     }, [community]);
-
-    // 이미지 업로드 DOM 선택
-    const fileInputRef = useRef<HTMLInputElement>(null);
-    const openFileInput = (type: string) => {
-        if(!myCommunity) return;
-        const fileInput = fileInputRef.current;
-        if(fileInput) {
-            fileInput.name = type;
-            fileInput.click();
-        }
-    }
 
     // 백엔드에 이미지 업로드 요청 보내기
     const uploadImage = async (event: ChangeEvent<HTMLInputElement>) => {
@@ -47,26 +58,19 @@ const CommunityPage: React.FC = () => {
             console.log(error);
         }
     }
+
     
-    // 커뮤니티별 페이지 데이터 가져오기
-    const fetcher = async (url: string) => {
-        try {
-            const res = await axios.get(url);
-            return res.data;
-        } catch (error: any) {
-            throw error.response.data
-        }
+    // 이미지 업로드 DOM 선택
+    const openFileInput = (type: string) => {
+        if(!myCommunity) return;
+        const fileInput = fileInputRef.current;
+        if(fileInput) {
+            fileInput.name = type;
+            fileInput.click();
+        } 
     }
-    const router = useRouter();
-    console.log(router);
 
-    // router에 query로 url정보가 담겨있으므로
-    // router.query { community: 'test1' }
-    const communityname = router.query.community;
 
-    
-    const { data: community, error } = useSWR(communityname ? `/communities/${communityname}` : null, fetcher);
-    console.log('community', community);
     return (
         <>
             {community &&
@@ -97,7 +101,7 @@ const CommunityPage: React.FC = () => {
                         {/* 커뮤니티 메타 데이터 */}
                         <div className='h-20 bg-white'>
                             <div className='relative flex max-w-5xl px-5 mx-auto'>
-                                {/* 커뮤니티 배너 이미지 UI */}
+                                {/* 커뮤니티 아이콘 이미지 UI */}
                                 <div className='absolute' style={{ top: -15 }}>
                                     {community.imageUrl && (
                                         <Image
@@ -106,7 +110,7 @@ const CommunityPage: React.FC = () => {
                                             width={70}
                                             height={70}
                                             className="rounded-full"
-                                            onClick={() => openFileInput("profile")}
+                                            onClick={() => openFileInput("image")}
                                         />
                                     )}
                                 </div>
@@ -122,7 +126,13 @@ const CommunityPage: React.FC = () => {
                             </div>
                         </div>
                     </div>
-                    </>
+
+                    {/* 포스트와 사이드바 */}
+                    <div className='flex max-w-5xl px-4 pt-5 mx-auto'>
+                        <div className="w-full md:mr-3 md:w-8/12"></div>
+                        <SideBar community={community} />
+                    </div>
+                </>
             }
         </>
     )
