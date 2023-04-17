cfd03052a7ecd460ee38132909ef96d7426ee41f
이준형 <dlwnsgud261@naver.com>
2023-04-17 23:47:27 +0900

    홈화면 UI 수정

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,12 +4,15 @@ import useSWR from 'swr';
 import axios from 'axios';
 import { Community } from '@/src/types';
 import Link from 'next/link';
+import { useAuthState } from '@/src/context/auth';
 
 const Home = () => {
+
+  const { authenticated } = useAuthState();
   const fetcher = async (url: string) => {
     return await axios.get(url).then(res => res.data)
   }
-  const address = `/subs/sub/topCommunity`;
+  const address = `/communities/community/topCommunities`;
   const { data: topCommunities } = useSWR<Community[]>(address, fetcher)
 
   return (
@@ -29,15 +32,46 @@ const Home = () => {
 
           {/* 커뮤니티 리스트 */}
           <div>
-           
-          <Link href="/communities/create">
+            {topCommunities?.map((community) => (
+              <div
+                key={community.name}
+                className="flex items-center px-4 py-2 text-xs border-b"
+              >
+                <Link href={`/c/${community.name}`}>
+                  
+                    <Image
+                      src={community.imageUrl}
+                      className="rounded-full cursor-pointer"
+                      alt="Community"
+                      width={24}
+                      height={24}
+                    />
+                  
+                </Link>
+                <Link href={`/c/${community.name}`}>
+                  
+                    {community.name}
+                  
+                </Link>
+                <p className='ml-auto font-md'>{community.postCount}</p>
+              </div>
+            ))}
+
+
+          </div>
+          {authenticated &&
+            <div className='w-full py-6 text-center'>
+              <Link href="/communities/select">
+                
+                  커뮤니티 만들기
                 
               </Link>
-                
+            </div>
+          }
+        </div>
               </div>
               </div>
-              </div>
-              </div>
+              
             
   )
 }
