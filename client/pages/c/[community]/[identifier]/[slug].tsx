1ce0e92ca5dcd8d9d1b1b4e7e7cedcf2ee2ad129
이준형 <dlwnsgud261@naver.com>
2023-04-17 23:44:01 +0900

    포스트 댓글 가져오기 및 좋아요 기능 생성

--- client/pages/c/[community]/[identifier]/[slug].tsx
+++ client/pages/c/[community]/[identifier]/[slug].tsx
@@ -6,6 +6,8 @@ import { FormEvent, useState } from "react";
 import { Post } from "@/src/types";
 import dayjs from "dayjs";
 import { useAuthState } from "@/src/context/auth";
+import { Comment } from "@/src/types";
+import classNames from "classnames";
 
 
 const PostPage = () => {
@@ -13,9 +15,12 @@ const PostPage = () => {
     const router = useRouter();
     const { identifier, community, slug } = router.query;
     // fetcher함수는 _app.tsx에서 관리하기
-    const { data: post, error} = useSWR<Post>(identifier && slug ? `/posts/${identifier}/${slug}` : null);
+    const { data: post, error, mutate: postMutate } = useSWR<Post>(identifier && slug ? `/posts/${identifier}/${slug}` : null);
     const { authenticated, user } = useAuthState();
     const [newComment, setNewComment] = useState("");
+    const { data: comments, mutate: commentMutate } = useSWR<Comment[]>(
+        identifier && slug ? `/posts/${identifier}/${slug}/comments` : null
+    )
 
     // 게시글에 대한 댓글 작성 핸들러
     const handleSubmit = async (e: FormEvent) => {
@@ -29,25 +34,76 @@ const PostPage = () => {
             await axios.post(`/posts/${post?.identifier}/${post?.slug}/comments`, {
                 body: newComment
             });
+            // 댓글 생성 요청 성공하면 mutate함수 호출해주기
+            commentMutate();
             // 댓글창 공란으로 만들기
             setNewComment("");
         } catch (error) {
             console.log(error);
         }
     }
+ /*
+    const like = async (value: number, comment?: Comment) => {
+        if (!authenticated) router.push("/login");
 
+        // 이미 클릭 한 like 버튼을 눌렀을 시에는 reset
+        if (
+            (!comment && value === post?.userLike) ||
+            (comment && comment.userLike === value)
+        ) {
+            value = 0
+        }
+
+        try {
+            await axios.post("/likes", {
+                identifier,
+                slug,
+                commentIdentifier: comment?.identifier,
+                value
+            })
+            postMutate();
+            commentMutate();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+*/
     return (
         <div className="flex max-w-5xl px-4 pt-5 mx-auto">
             <div className="w-full md:mr-3 md:w-8/12">
                 <div className="bg-white rounded">
                     {post && (
-                        <>
+                        <>  
+                            <div className="flex">
+                                {/* 게시글에 대한 좋아요 버튼 부분 */}
+                                <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                                    {/* 좋아요 */}
+                                    <div
+                                        className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                                        // onClick={() => like(1)}
+                                    >
+                                            { /* post.userLike가 1일때 text-red-500 렌더링되게 하기 */}
+                                            <i className={classNames("fas fa-arrow-up", {"text-red-500": post.userLike === 1})} >
+                                            </i>
+                                        
+                                    </div>
+                                    <p className="text-xs font-bold">{post.likeScore}</p>
+                                    {/* 싫어요 */}
+                                    <div
+                                        className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                                        // onClick={() => like(-1)}
+                                    >
+                                        <i className={classNames("fas fa-arrow-down", {"text-blue-500": post.userLike === -1})} >
+                                        </i>
+                                    </div>
+                                </div>
+                            </div>
                             
                                 <div className="mt-10 py-2 pr-2">
                                     <div className="flex items-center">
                                         <p className="text-xs test-gray-400">
                                             Posted by                <i className="fas fa-abacus"></i>      
-                                            <Link href={`/u/${post.username}`}>
+                                            <Link href={`/c/${post.username}`}>
                                                 <span className="mx-1 hover:underline">
                                                     /u/{post.username}
                                                 </span>
@@ -78,7 +134,7 @@ const PostPage = () => {
                                 {authenticated ?
                                     (<div>
                                         <p className="mb-1 text-xs">
-                                            <Link href={`/u/${user?.username}`}>
+                                            <Link href={`/c/${user?.username}`}>
                                                 <span className="font-semibold text-blue-500">
                                                     {user?.username}
                                                 </span>
@@ -123,7 +179,49 @@ const PostPage = () => {
                                     
                             
                             {/* 댓글 리스트 부분 */}
-                            
+                            {comments?.map(comment => (
+                                <div className="flex" key={comment.identifier}>
+                                    {/* 댓글에 대한 좋아요 버튼 부분 */}
+                                    <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                                        {/* 좋아요 */}
+                                        <div
+                                            className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                                            //onClick={() => like(1, comment)}
+                                        >
+                                            <i className={classNames("fas fa-arrow-up", {"text-red-500": comment.userLike === 1})} >
+                                            </i>
+                                        </div>
+                                        <p className="text-xs font-bold">{comment.likeScore}</p>
+                                        {/* 싫어요 */}
+                                        <div
+                                            className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                                            //onClick={() => like(-1, comment)}
+                                        >
+                                             <i className={classNames("fas fa-arrow-down", {"text-blue-500": comment.userLike === -1})} >
+                                            </i>
+                                        </div>
+                                    </div>
+
+                                    <div className="py-2 pr-2">
+                                        <p className="mb-1 text-xs leading-none">
+                                            <Link href={`/c/${comment.username}`}>
+                                                <span className="mr-1 font-bold hover:underline">
+                                                    {comment.username}
+                                                </span>
+                                            </Link>
+                                            <span className="text-gray-600">
+                                                {`
+                                              ${comment.likeScore}
+                                              posts
+                                              ${dayjs(comment.createdAt).format("YYYY-MM-DD HH:mm")}
+                                            `}
+                                            </span>
+                                        </p>
+                                        <p>{comment.body}</p>
+                                    </div>
+                                </div>
+                            ))}
+
                             
                             
 
