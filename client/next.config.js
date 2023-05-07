b8ee14ec785495f11b85a90b55a87fc5ff80b90e
이준형 <dlwnsgud261@naver.com>
2023-05-07 17:24:02 +0900

    feat: swcMinify 속성 제거

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, 
+  //swcMinify: true,
+  images: {
+    // 이미지 업로드 관련 domain 설정
+    domains: [
+      "www.gravatar.com", "localhost"
+    ]
+  }
 }
 
 module.exports = nextConfig
