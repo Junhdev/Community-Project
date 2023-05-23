56f66b990f9995d05c590724a560281920a96b04
이준형 <dlwnsgud261@naver.com>
2023-05-23 23:56:46 +0900

    feat: useModal 훅 생성

---
+++ client/src/hooks/useModal.ts
@@ -0,0 +1,34 @@
+import { useCallback } from "react";
+import { useRecoilState } from "recoil";
+import { modalState } from "../atoms/modalAtom";
+
+type OpenModalType = {
+    title: string;
+    content: JSX.Element | string;
+    callback?: () => any;
+  };
+  
+  export const useModal = () => {
+    const [modalDataState, setModalDataState] = useRecoilState(modalState);
+  
+    const closeModal = useCallback(
+      () =>
+        setModalDataState((prev) => {
+          return { ...prev, isOpen: false };
+        }),
+      [setModalDataState]
+    );
+  
+    const openModal = useCallback(
+      ({ title, content, callback }: OpenModalType) =>
+        setModalDataState({
+          isOpen: true,
+          title: title,
+          content: content,
+          callBack: callback
+        }),
+      [setModalDataState]
+    );
+  
+    return { modalDataState, closeModal, openModal };
+  };
